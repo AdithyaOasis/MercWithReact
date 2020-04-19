cb032a449ee64826ff1fd057d8be0d9cbd4ce403
AdithyaOasis <adithya.oasis@gmail.com>
2020-04-19 12:37:55 +0530

    w/ Todo

--- Mercury/frontend/src/components/pages/Clubs/Home.js
+++ Mercury/frontend/src/components/pages/Clubs/Home.js
@@ -1,11 +1,19 @@
 import React, { Component, Fragment } from "react";
 import Header_Clubs from "../../layout/Header_Clubs";
+import Todoapp from "../../todo/Todoapp";
 export class Home extends Component {
   render() {
     return (
       <Fragment>
         <Header_Clubs />
         <h1>Clubs</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-3-sm">
+              <Todoapp />
+            </div>
+          </div>
+        </div>
       </Fragment>
     );
   }

cf435fe2ad513d6f2b857a4dd9c47e6938e85592
I A D I <43103947+AdithyaOasis@users.noreply.github.com>
2020-04-20 21:29:59 +0530

    Update words.py

--- Mercury/Mercury/words.py
+++ Mercury/Mercury/words.py
@@ -1,7 +1,7 @@
 
 # Specify your templates directory path here
 temPlates = 'C:/django-1/Mercury/templates'
-pword = 'oasis123'  # Use this to specify your db password
+pword = 'password'  # Use this to specify your db password
 uSer = 'postgres'  # Postgres db user,
 hOst = 'localhost'  # Recommended to use this itself
 pOrt = '5432'  # Incase you have the port already being used, change

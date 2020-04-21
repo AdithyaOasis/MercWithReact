7911a7c96c8f3ed781d8b430368eb9e2c4b3b36a
AdithyaOasis <adithya.oasis@gmail.com>
2020-04-21 23:20:55 +0530

    Settings.py no templates

--- Mercury/Mercury/settings.py
+++ Mercury/Mercury/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from Mercury.words import hOst, temPlates, uSer, pword, pOrt
+from Mercury.words import hOst, uSer, pword, pOrt
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
